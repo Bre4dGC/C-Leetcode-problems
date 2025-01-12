a5aa30f08cf7857be25516b83682f293000c6bfb
Bre4dGC <glebbreadc@gmail.com>
2025-01-12 13:07:08 +0300

    solved 9 problems

--- leetcode_problems/Program.cs
+++ leetcode_problems/Program.cs
@@ -1,4 +1,110 @@
-﻿Console.WriteLine(LengthOfLastWord("Hello World"));
+﻿using System.Text;
+
+// 1400. Construct K Palindrome Strings
+bool CanConstruct(string s, int k)
+{
+    if (k > s.Length)
+        return false;
+    int[] freq = new int[26];
+    foreach (char c in s)
+        freq[c - 'a']++;
+    int oddCount = 0;
+    foreach (int count in freq)
+        if (count % 2 != 0)
+            oddCount++;
+    return oddCount <= k;
+}
+
+//68. Text Justification - решено
+IList<string> FullJustify(string[] words, int maxWidth)
+{
+    IList<string> result = new List<string>();
+    List<string> lineWords = new List<string>();
+    int lineLength = 0;
+    foreach (var word in words)
+    {
+        if (lineLength + word.Length + lineWords.Count > maxWidth)
+        {
+            result.Add(JustifyLine(lineWords, lineLength, maxWidth));
+            lineWords.Clear();
+            lineLength = 0;
+        }
+        lineWords.Add(word);
+        lineLength += word.Length;
+    }
+    if (lineWords.Count > 0)
+    {
+        string lastLine = string.Join(" ", lineWords).PadRight(maxWidth);
+        result.Add(lastLine);
+    }
+    return result;
+}
+string JustifyLine(List<string> words, int lineLength, int maxWidth)
+{
+    if (words.Count == 1)
+        return words[0].PadRight(maxWidth);
+    int totalSpaces = maxWidth - lineLength;
+    int spacesBetween = totalSpaces / (words.Count - 1);
+    int extraSpaces = totalSpaces % (words.Count - 1);
+    StringBuilder justifiedLine = new StringBuilder();
+    for (int i = 0; i < words.Count; i++)
+    {
+        justifiedLine.Append(words[i]);
+        if (i < words.Count - 1)
+            justifiedLine.Append(' ', spacesBetween + (i < extraSpaces ? 1 : 0));
+    }
+    return justifiedLine.ToString();
+}
+
+
+string ReplaceDigits(string s)
+{
+    char ch = ' ';
+    string result = "";
+    foreach (var item in s)
+    {
+        if (char.IsDigit(item))
+            result += (char)(ch + (item - '0'));
+        else
+        {
+            ch = item;
+            result += item;
+        }
+    }
+    return result;
+}
+
+string LongestCommonPrefix1(string[] strs)
+{
+    string prefix = "";
+    int index = 0;
+    foreach (var s in strs)
+    {
+        prefix += s[index];
+        for (int i = 0; i < strs.Length; i++)
+            if (!strs[i].StartsWith(prefix))
+                return prefix = prefix[..^1];
+        index++;
+    }
+    return "";
+}
+// 14. Longest Common Prefix - решено
+string LongestCommonPrefix2(string[] strs)
+{
+    if (strs == null || strs.Length == 0)
+        return "";
+    string prefix = strs[0];
+    foreach (string s in strs)
+        while (!s.StartsWith(prefix))
+        {
+            prefix = prefix[..^1];
+            if (string.IsNullOrEmpty(prefix))
+                return "";
+        }
+    return prefix;
+}
+
+
 // 1. Two Sum - решено
 int[] TwoSum(int[] nums, int target)
 {
@@ -18,6 +124,11 @@ bool IsPalindrome(int x)
     return new string(reversed) == palindrome;
 }
 
+int MySqrt(int x)
+{
+    return (int)Math.Sqrt(x);
+}
+
 // 27. Remove Element - решено
 int RemoveElement(int[] nums, int val)
 {
@@ -28,7 +139,7 @@ int RemoveElement(int[] nums, int val)
     return resultLength;
 }
 
-// 26. Remove Duplicates from Sorted Array
+// 26. Remove Duplicates from Sorted Array - решено
 int RemoveDuplicates(int[] nums)
 {
     int count = 0;
@@ -38,11 +149,84 @@ int RemoveDuplicates(int[] nums)
     return count;
 }
 
-//58. Length of Last Word
+// 58. Length of Last Word - решено
 int LengthOfLastWord(string s)
 {
     string[] words = s.Trim().Split(' ');
     return words[^1].Length;
+}
+
+// 916. Word Subsets - не решено
+IList<string> FindUniversalWords(string[] words1, string[] words2)
+{
+    IList<string> result = new List<string>();
+    int[] maxFreq = new int[26];
+    foreach (string word in words2)
+    {
+        int[] freq = CalculateFrequency(word);
+        for (int i = 0; i < 26; i++)
+            maxFreq[i] = Math.Max(maxFreq[i], freq[i]);
+    }
+    foreach (string word in words1)
+    {
+        int[] freq = CalculateFrequency(word);
+        if (IsUniversal(freq, maxFreq))
+            result.Add(word);
+    }
+    return result;
+}
+
+int[] CalculateFrequency(string word)
+{
+    int[] freq = new int[26];
+    foreach (char c in word)
+        freq[c - 'a']++;
+    return freq;
+}
+
+bool IsUniversal(int[] freq, int[] maxFreq)
+{
+    for (int i = 0; i < 26; i++)
+        if (freq[i] < maxFreq[i])
+            return false;
+    return true;
+}
+
+
+bool IsPowerOfFour(int n)
+{
+    return n == Math.Pow(4, n);
+}
+
+// 747. Largest Number At Least Twice of Others
+int DominantIndex(int[] nums)
+{
+    int highestNum = int.MinValue;
+    int index = 0;
+    for (int i = 0; i < nums.Length; i++)
+        if (nums[i] > highestNum) 
+        { 
+            highestNum = nums[i];
+            index = i;
+        }
+    foreach (var item in nums)
+        if (item != highestNum && highestNum < item * 2) return -1;
+    return index;
+}
+
+// 28. Find the Index of the First Occurrence in a String - решено
+int StrStr(string haystack, string needle)
+{
+    for (int i = 0; i < haystack.Length; i++)
+    {
+        string temp = "";
+        if(i + needle.Length <= haystack.Length)
+            for (int j = i; j < needle.Length + i; j++)
+                temp += haystack[j];
+        if(needle == temp)
+            return i;
+    }
+    return -1;
 }
 
 // 3340. Check Balanced String - решено
@@ -60,7 +244,7 @@ bool isBalanced(string num)
     return even == odd;
 }
 
-// 2185. Counting Words With a Given Prefix
+// 2185. Counting Words With a Given Prefix - решено
 int PrefixCount(string[] words, string pref)
 {
     int prefCount = 0;
@@ -132,21 +316,30 @@ int FindGCD(int[] nums)
     return gcd;
 }
 
-// Check binaries - решено
+// 1784. Check if Binary String Has at Most One Segment of Ones - решено
 bool CheckOnesSegment(string s)
 {
-    return s.Contains("11");
+    return !s.Contains("01");
 }
 
+// 34. Find First and Last Position of Element in Sorted Array - решено
 int[] SearchRange(int[] nums, int target)
 {
-    int[] range = new int[2];
+    int startRange = -1, endRange = -1;
     for (int i = 0; i < nums.Length; i++)
-    {
         if (nums[i] == target)
-            range[0] += i;
-    }
-    return range;
+        {
+            if (startRange == -1)
+                startRange = i;
+            endRange = i;
+        }
+    return [startRange, endRange];
+}
+
+int SearchInsert(int[] nums, int target)
+{
+    int index = 0;    
+    return index;
 }
 
 // 506. Relative Ranks - решено
